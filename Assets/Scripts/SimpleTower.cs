1511ca5d4f4e6f3ff49a37b5e9ceb68af15742f4
Maciej Hyla <maciejhyla95@gmail.com>
2020-06-06 12:54:10 +0200

    Passing enemies using ref
    Take into consideration new spawned enemies

--- Assets/Scripts/SimpleTower.cs
+++ Assets/Scripts/SimpleTower.cs
@@ -15,7 +15,7 @@
 
         private IReadOnlyList<Enemy> enemies;
 
-        public void Initialize(IReadOnlyList<Enemy> enemies)
+        public void Initialize(ref IReadOnlyList<Enemy> enemies)
         {
             this.enemies = enemies;
             fireTimer = firingRate;
